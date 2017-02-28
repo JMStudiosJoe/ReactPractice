ef05d616128bec0a5781e6a7ccf4f43ec5b22619
Joseph <jrichardson@jmstudios.net>
2017-02-27 19:38:47 -0800

    have a working gulpfile just run gulp and default task does it, need to understand but then you need to manually open the index.html file

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,73 @@ var uglify = require('gulp-uglify');
 // var htmlReplace = require('gulp-html-replace');
 
 var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var tslint = require("gulp-tslint");
 
+var tsProject = ts.createProject('tsconfig.json');
+var tsSrc = ['src/**/*.ts', 'src/**/*.tsx'];
+
+var watchify = require("watchify");
+var gutil = require("gulp-util");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
 
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/tsx/app.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
 
 var path = {
     HTML: 'index.html',
-    ALL: ['src/tsx/*.tsx', 'src/tsx/**/*.tsx', 'index.html'],
-    TS: ['src/tsx/*.tsx', 'src/tsx/**/*.tsx'],
+    ALLHTML: 'src/html/*.html',
+    ALL: ['src/*.ts', 'src/**/*.ts', 'index.html'],
+    TS: ['src/*.ts', 'src/**/*.ts'],
     MINIFIED_OUT: 'build/build.min.js',
     DEST_SRC: 'build/dist/src',
     DEST_BUILD: 'build/dist/build',
     DEST: 'build/dist'
 };
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('build'));
+}
 
-gulp.task('default', function(){
-    console.log("hello gulp");
+gulp.task('copy-html', function () {
+    return gulp.src(path.HTML)
+        .pipe(gulp.dest('build'));
 });
+gulp.task("default", ["copy-html"], bundle);
+// gulp.task('default', ['copy-html'], function () {
+//     return browserify({
+//         basedir: '.',
+//         debug: true,
+//         entries: ['src/tsx/app.ts'],
+//         cache: {},
+//         packageCache: {}
+//     })
+//     .plugin(tsify)
+//     .bundle()
+//     .pipe(source('bundle.js'))
+//     .pipe(gulp.dest('build'));
+// });
 
-
-gulp.task("tsbuild", function () {
+gulp.task("ts:build", function() {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest("build"));
+        .js
+        .pipe(gulpConcat('bundle.js'))
+        .pipe(gulp.dest('build'));
 });
+
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
+// gulp.task("tsbuild", function () {
+//     return tsProject.src()
+//         .pipe(tsProject())
+//         .js.pipe(gulp.dest("build"));
+// });
--- src/tsx/helloComponent.ts
+++ src/tsx/helloComponent.ts
@@ -1,13 +1,16 @@
-import * as React from "react";
-
-interface HelloProps {
-  name: string;
+// import * as React from "react";
+//
+// interface HelloProps {
+//   name: string;
+// }
+//
+// class HelloJoey extends React.Component<HelloProps, {}> {
+//   render() {
+//     return <div>Hello, {this.props.name}</div>;
+//   }
+// }
+//
+// export default HelloJoey;
+export function sayHello(name: string) {
+    return `Hello from ${name}`;
 }
-
-class HelloJoey extends React.Component<HelloProps, {}> {
-  render() {
-    return <div>Hello, {this.props.name}</div>;
-  }
-}
-
-export default HelloJoey;
