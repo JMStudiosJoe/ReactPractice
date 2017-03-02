fff6b2b1bf40a6aec0664b133476028b58768bb2
Joseph <jrichardson@jmstudios.net>
2017-03-01 16:34:17 -0800

    adding in the one import will break things where remove import things work again

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var nodeExternals = require('webpack-node-externals');
 module.exports = {
     entry: "./src/app.tsx",
     output: {
@@ -11,25 +10,24 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".tsx"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
-		loaders: [
-			{
-				test: /\.tsx?$/,
-				loader: "awesome-typescript-loader",
-				exclude: /(node_modules|__tests__)/
-			}
-		]
-	},
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, exclude: /node_modules/, loader: "awesome-typescript-loader" }
+        ]
+    },
 
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
 
-    target: 'node', // in order to ignore built-in modules like path, fs, etc.
-    externals: [nodeExternals(), "node_modules"]
+    },
 
 };
