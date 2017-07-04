aa0fce383a35911ee6d4cf1ba2878953f28d42d9
Joseph <jrichardson@jmstudios.net>
2017-07-04 13:52:11 -0700

    looking into the catch all options for node server tis an interesting problem

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors()); what does it do?
 app.use(publicPath);
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     console.log(req.rawHeaders[9]);
     res.sendFile(indexPath)
 });
