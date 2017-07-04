8876dcbfdc9770aee190a22986b9c8f4f3050e18
Joseph <jrichardson@jmstudios.net>
2017-07-03 19:33:09 -0700

    removed reactBuild

--- reactBuild/src/redux/store/store.tsx
+++
@@ -1,8 +0,0 @@
-
-import * as redux from 'redux';
-import {LoginReducer} from '../reducers/loginReducers';
-import {VoteSmartReducer} from '../reducers/voteSmartReducers';
-const combinedReducers = redux.combineReducers({VoteSmartReducer, LoginReducer});
-const store = redux.createStore(combinedReducers);
-
-export default store;
