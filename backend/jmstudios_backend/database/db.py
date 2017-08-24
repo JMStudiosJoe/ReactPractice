1cc222dbb2019f5fe0faaf86506bb6c3028576e4
Joseph <jrichardson@jmstudios.net>
2017-08-24 16:46:32 -0700

    was able to commit new team member in debug mode but for some reason the session methods are not working as hoped might need to dig a little further

--- backend/jmstudios_backend/database/db.py
+++ backend/jmstudios_backend/database/db.py
@@ -1,5 +1,10 @@
 import sqlalchemy
 from jmstudios_backend.database.engine import Engine
+from sqlalchemy.orm import sessionmaker
+from jmstudios_backend.database.models.team_member import TeamMember
+
+Session = sessionmaker()
+session = None
 
 def connect(user, password, db, host='localhost', port=5432):
     url = 'postgresql://{}:{}@{}:{}/{}'
@@ -7,5 +12,23 @@ def connect(user, password, db, host='localhost', port=5432):
     print(url)
     con = sqlalchemy.create_engine(url, client_encoding='utf8')
     meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+    Session.configure(bind=con)
+    session = Session()
+    member = TeamMember(first_name='joseph')
+    import pdb; pdb.set_trace()
+
     db_engine = Engine(meta, con)
     return con, meta
+
+
+def get_session():
+    return session
+
+
+def add_to_session(data):
+    session.add(data)
+
+
+def commit_session():
+    session.commit()
--- backend/jmstudios_backend/database/engine.py
+++ backend/jmstudios_backend/database/engine.py
@@ -1,10 +1,10 @@
 from jmstudios_backend.database.base import Base
 from jmstudios_backend.database.models.team_member import TeamMember
+from jmstudios_backend.database import db
 
 class Engine():
     def __init__(self, meta, con):
         self.meta = meta
         self.con = con
-        import pdb;pdb.set_trace()
 
 
