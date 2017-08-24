855737b49e8a00cc6887c616f119c8fa35298351
Joseph <jrichardson@jmstudios.net>
2017-08-23 17:30:06 -0700

    better styling

--- src/components/sampleProject/displayElection.tsx
+++ src/components/sampleProject/displayElection.tsx
@@ -4,23 +4,63 @@ import { Election } from '../types/voteSmartTypes'
 import InfiniteCalendar from 'react-infinite-calendar'
 import 'react-infinite-calendar/styles'
 
+const electionContainer = () => {
+    return style.create({
+        
+    })
+}
+const electionHeader = () => {
+    return style.create({
+        textAlign: 'center',
+        display: 'block',
+
+    })
+}
+const electionVotingInfoContainer = () => {
+    return style.create({
+
+    })
+}
+const electionCalendar = () => {
+    return style.create({
+        textAlign: 'center',
+        display: 'inline-block',
+
+    })
+}
+const voterInformation = () => {
+    return style.create({
+        textAlign: 'center',
+        display: 'inline-block',
+
+    })
+}
 
 const displayElection = (election: Election) => {
-    var today = new Date()
+    var today = new Date(election.electionDay)
     var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7)
+
     return (
-        <div >
-            {election.electionDay}
-            <br />
-            {election.name}
-            <br />
-            <InfiniteCalendar
-                width={400}
-                height={600}
-                selected={today}
-                disabledDays={[0,6]}
-                minDate={lastWeek}
-            />
+        <div style={electionContainer()} >
+            <div style={electionHeader()}>
+                {election.electionDay}
+                <br />
+                {election.name}
+                <br />
+            </div>
+            <div style={electionVotingInfoContainer()}>
+                <div style={electionCalendar()}>
+                    <InfiniteCalendar
+                        width={300}
+                        height={260}
+                        selected={today}
+                        minDate={lastWeek}
+                    />
+                </div>
+                <div style={voterInformation()}>
+                    voter info
+                </div>
+            </div>
         </div>
     )
 }
