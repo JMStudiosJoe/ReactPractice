9e1f7d8f51aea28c6852cf8b215e74cb756bda5b
Joseph <jrichardson@jmstudios.net>
2017-09-03 17:08:43 -0700

    able to upload to s3 now going to make the images and store to db

--- backend/jmstudios_backend/database/models/images.py
+++ backend/jmstudios_backend/database/models/images.py
@@ -12,6 +12,11 @@ class Image(Base):
     image_url = Column(String(100))
 
     @classmethod
+    def create_new_image(cls, name, type, path):
+        print(type)
+        cls.upload_image_to_s3(name, path)
+
+    @classmethod
     def get_by_id(cls, image_id):
         image = Session.get_session().query(cls).filter(self.id == image_id).first()
         return image.json()
@@ -21,6 +26,22 @@ class Image(Base):
         images = Session.get_session().query(cls).all()
         json_images = [image.json() for image in images]
         return json_images
+
+    @classmethod
+    def upload_image_to_s3(cls, name, path):
+        import boto3
+        bucket_name = 'jmstudiosimages'
+        s3 = boto3.resource('s3')
+        try:
+            s3.meta.client.upload_file(path, bucket_name, name)
+            object_acl = s3.ObjectAcl(bucket_name, name)
+            response = object_acl.put(ACL='public-read')
+        except e:
+            print(e)
+
+    @classmethod
+    def generate_s3_url(cls, name, bucket_name):
+        return 'https://s3-us-west-2.amazonaws.com/{}/{}'.format(bucket_name, name)
 
     def json(self):
         return {
--- backend/jmstudios_backend/db_setup_scripts/db_setup.py
+++ backend/jmstudios_backend/db_setup_scripts/db_setup.py
@@ -1,7 +1,27 @@
 from jmstudios_backend.database.base import Base
-
+from jmstudios_backend.database.models.team_member import TeamMember
+from jmstudios_backend.database.models.images import Image
 from jmstudios_backend.database import db
+from jmstudios_backend.db_setup_scripts.setup_data.image_data import images
 
 
 meta, con = db.connect('jmstudios', 'jmstudios', 'jmstudios')
-import pdb;pdb.set_trace()
+image_path = 'jmstudios_backend/db_setup_scripts/setup_data/autum_trees.jpg'
+
+#Base.metadata.drop_all()
+#Base.metadata.create_all()
+
+def initialize_team_members():
+    joseph = dict(
+        first_name='Joseph',
+        last_name='Richardson',
+        title='Co Founder',
+        description='',
+        linked_in_url='',
+        github_url=''
+        #image=Image()
+    )
+
+def initialize_images():
+    for data in images:
+        Image.create_new_image(**data)
